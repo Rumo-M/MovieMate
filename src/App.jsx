a7d27fc95450b29c5a2f80c966b26aa1288cc48f
Martha Maloba <marthamarumo92@gmail.com>
2025-09-26 22:30:39 +0200

    Modify App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,9 @@
 import React from "react";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-
-import Navbar from "./components/Navbar";
-import Footer from "./components/Footer";
-
-import Home from "./pages/Home";
-import Search from "./pages/Search";
-import MovieDetail from "./pages/MovieDetail";
-import Watchlist from "./pages/Watchlist";
-import Profile from "./pages/Profile";
 
 export default function App() {
   return (
-    <Router>
-      {/* Flex column to keep footer at bottom */}
-      <div className="flex flex-col min-h-screen bg-gray-900 text-white">
-        <Navbar />
-
-        {/* Main content */}
-        <main className="flex-1">
-          <Routes>
-            <Route path="/" element={<Home />} />
-            <Route path="/search" element={<Search />} />
-            <Route path="/movie/:id" element={<MovieDetail />} />
-            <Route path="/watchlist" element={<Watchlist />} />
-            <Route path="/profile" element={<Profile />} />
-          </Routes>
-        </main>
-
-        <Footer />
-      </div>
-    </Router>
+    <div className="bg-gray-900 text-white min-h-screen flex items-center justify-center">
+      <h1 className="text-3xl font-bold">MovieMate App</h1>
+    </div>
   );
 }
