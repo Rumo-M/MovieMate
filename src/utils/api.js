f00cfa5f9a1eab4f3bdd928bf1867648157b525e
Martha Maloba <marthamarumo92@gmail.com>
2025-09-26 23:22:55 +0200

    Modify api.js

--- src/utils/api.js
+++ src/utils/api.js
@@ -1,9 +1,10 @@
 import axios from "axios";
 
+// Create an Axios instance for TMDb API
 const api = axios.create({
-  baseURL: "https://api.themoviedb.org/3/",
+  baseURL: "https://api.themoviedb.org/3",
   params: {
-    api_key: import.meta.env.VITE_TMDB_API_KEY,
+    api_key: import.meta.env.VITE_TMDB_API_KEY, // your TMDb API key from .env
   },
 });
 
