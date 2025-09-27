68735c42f8129f26f0d7ac92e625dc9d3a78bd36
Martha Maloba <marthamarumo92@gmail.com>
2025-09-27 13:15:39 +0200

    Create mockMovies.js

---
+++ src/utils/mockMovies.js
@@ -0,0 +1,22 @@
+const mockMovies = [
+  {
+    id: 1,
+    title: "Inception",
+    poster_path: "/9gk7adHYeDvHkCSEqAvQNLV5Uge.jpg",
+    vote_average: 8.8,
+    release_date: "2010-07-16",
+    vote_count: 29000,
+    genre_ids: [28, 878, 12],
+  },
+  {
+    id: 2,
+    title: "The Dark Knight",
+    poster_path: "/qJ2tW6WMUDux911r6m7haRef0WH.jpg",
+    vote_average: 9.0,
+    release_date: "2008-07-18",
+    vote_count: 34000,
+    genre_ids: [18, 28, 80],
+  },
+];
+
+export default mockMovies;
